685064452b4ef0ace6270bf004c5ebf5f6d18557
Ryan Gardner <rgardner1727@gmail.com>
2024-09-22 11:22:38 -0400

    Added content of folder related to Spring Security that was not previously being tracked

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.in28minutes.springboot.myfirstwebapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager createInMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(User.builder()
+                .passwordEncoder(input -> passwordEncoder().encode(input))
+                .username("in28minutes")
+                .password("password")
+                .roles("USER", "ADMIM")
+                .build());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
