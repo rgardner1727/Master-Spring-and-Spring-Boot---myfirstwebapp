c2c08e8e2300c683ea239fd805e4c7901b0e47d5
Ryan Gardner <rgardner1727@gmail.com>
2024-09-23 15:09:10 -0400

    extracted creation of UserDetails to its own method to easily create new users

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,16 +14,21 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createInMemoryUserDetailsManager(){
-        return new InMemoryUserDetailsManager(User.builder()
-                .passwordEncoder(input -> passwordEncoder().encode(input))
-                .username("in28minutes")
-                .password("password")
-                .roles("USER", "ADMIN")
-                .build());
+        return new InMemoryUserDetailsManager(createNewUser("in28minutes", "password"),
+                createNewUser("rgardner", "password"));
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private UserDetails createNewUser(String username, String password){
+        return User.builder()
+                .passwordEncoder(input -> passwordEncoder().encode(input))
+                .username(username)
+                .password(password)
+                .roles("USER", "ADMIN")
+                .build();
     }
 }
